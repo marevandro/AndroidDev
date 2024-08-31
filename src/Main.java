f6413dd48914fcbfcd7cddc5228534addfe27d24
marevandro <marevandro95@gmail.com>
2024-08-31 15:50:21 -0300

    Métodos Get, Set e Field

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        Planet p = new Planet();
+        System.out.println(p.getNone());
+        p.setNone("Planeta Terra");
+        System.out.println(p.getNone());
+    }
+}
